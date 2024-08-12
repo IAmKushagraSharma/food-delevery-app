c37ef4f939d6055164d3ec67f140df03c56f179a
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-08-12 23:39:46 +0530

    TailwindCSS init
    - Restructured the project
    - Added tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * Main entry: Higher level root component
+ * Only create minimal functionality here, distribute functionality to
+ * specific components, which you'll create in the src/components directory.
+ */
+
+function App() {
+  return (
+    <>
+      <div className="border-4 w-40 p-6 text-center mt-40 mx-auto">
+        Tailwind init
+      </div>
+    </>
+  );
+}
+
+export default App;
